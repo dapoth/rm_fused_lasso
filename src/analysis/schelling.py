abb43037b28e1869dfb3437c33d70f921c64ac62
Christopher Thiemann <christopherth94@googlemail.com>
2019-02-15 15:57:28 +0100

    Modified the 'analysis' directory to include our files.

--- src/analysis/schelling.py
+++ src/analysis/schelling.py
@@ -13,106 +13,118 @@ import json
 import logging
 import pickle
 import numpy as np
+import cvxpy as cp
 
 from src.model_code.agent import Agent
 from bld.project_paths import project_paths_join as ppj
 
 
-def setup_agents(model):
-    """Load the simulated initial locations and return a list
-    that holds all agents.
-
-    """
-
-    initial_locations = np.loadtxt(ppj("OUT_DATA", "initial_locations.csv"), delimiter=",")
-    initial_locations = initial_locations.reshape(2, model["n_types"], 30000)
-
-    agents = []
-    for typ in range(model["n_types"]):
-        for i in range(model["n_agents_by_type"][typ]):
-            agents.append(
-                Agent(
-                    typ=typ,
-                    initial_location=initial_locations[typ, :, i],
-                    n_neighbours=model["n_neighbours"],
-                    require_same_type=model["require_same_type"],
-                    max_moves=model["max_moves"]
-                )
-            )
-
-    return agents
 
 
-def _get_locations_by_round_dict(model):
-    """Return a dictionary with arrays to store locations for each type."""
-    return {
-        typ: np.zeros((model["n_agents_by_type"][typ], 2)) * np.nan
-        for typ in range(model["n_types"])
-    }
+def solution_path_unconstraint(y,x,lambda1=0,lambda2=0):
+    
+    ### "from constraint import constraint" to import function
+    ### y and x data as usual
+    ### lambda1 and lambda2 optional to make vertical line in the plot
+    
+    p = len(x[1,:])
+    gamma1 = cp.Parameter(nonneg=True)
+    gamma2 = cp.Parameter(nonneg=True)
+    b = cp.Variable(p)
+    error = cp.sum_squares(x*b - y)
+    obj = cp.Minimize(error+gamma1*cp.norm(b,1)  +gamma2*cp.norm(b[1:p]-b[0:p-1],1))
+    prob = cp.Problem(obj)
+    
+    
+    x_values = []
+    gamma_vals = np.logspace(-2, 6)
+    for val in gamma_vals:
+        gamma1.value = val
+        gamma2.value = lambda2
+        prob.solve()
+        x_values.append(b.value)
+    
+    x2_values = []    
+    gamma2_vals = np.logspace(-2,6)
+    for val in gamma_vals:
+        gamma1.value = lambda1
+        gamma2.value = val
+        prob.solve()
+        x2_values.append(b.value)
+    
+    plt.rc('text', usetex=True)
+    plt.rc('font', family='serif')
+    plt.figure(figsize=(6,10))
+    
+    # Plot entries of x vs. lambda1.
+    plt.subplot(211)
+    plt.axvline(x=lambda1)
+    for i in range(p):
+        plt.plot(gamma_vals, [xi[i] for xi in x_values])
+    plt.xlabel(r'$\lambda_1$', fontsize=16)
+    plt.ylabel(r'$\beta_{i}$', fontsize=16)
+    plt.xscale('log')
+    plt.title(r'Entries of $\beta$ vs. $\lambda_1$')
+    
+    plt.subplot(212)
+    plt.axvline(x=lambda2)
+    for i in range(p):
+        plt.plot(gamma2_vals, [xi[i] for xi in x2_values])
+    plt.xlabel(r'$\lambda_2$', fontsize=16)
+    plt.ylabel(r'$\beta_{i}$', fontsize=16)
+    plt.xscale('log')
+    plt.title(r'Entries of $\beta$ vs. $\lambda_2$')
+    
+    plt.tight_layout()
+    plt.show()
+    
+    
+    
+    
+    #prob.solve()
+    
+    return print("The prcoess was",prob.status)
+
+def fused_lasso_primal(y,x,s1,s2):
+    
+    ### "from constraint import un_constraint" to import function
+    ### y and x data as usual
+    
+    p = len(x[1,:])
+    b = cp.Variable(p)
+    error = cp.sum_squares(x*b - y)
+    obj = cp.Minimize(error)
+    constraints = [cp.norm(b,1) <= s1, cp.norm(b[1:p]-b[0:p-1],1) <= s2]
+    prob = cp.Problem(obj, constraints)
+    prob.solve()
+    
+    return b.value
 
 
-def _store_locations_by_round(loc, agents):
-    """Update the dictionary *loc* with the locations of each agent.
-
-    Doing so is a bit tedious because we do so by type.
-
-    """
-
-    counter = {0: 0, 1: 0}
-    for agent in agents:
-        typ = agent.type
-        loc[typ][counter[typ], :] = agent.location
-        counter[typ] += 1
 
 
-def run_analysis(agents, model):
-    """Given an initial set of *agents* and the *model*'s parameters,
-    return a list of dictionaries with *type: N x 2* items.
-
-    """
-
-    locations_by_round = [_get_locations_by_round_dict(model)]
-    _store_locations_by_round(locations_by_round[-1], agents)
-
-    for loop_counter in range(model["max_iterations"]):
-        logging.info("Entering loop {}".format(loop_counter))
-        # Make room for locations.
-        locations_by_round.append(_get_locations_by_round_dict(model))
-        # Update locations as necessary
-        someone_moved = False
-        for agent in agents:
-            old_location = agent.location
-            # If necessary, move around until happy
-            agent.move_until_happy(agents)
-            if not (agent.location == old_location).all():
-                someone_moved = True
-        _store_locations_by_round(locations_by_round[-1], agents)
-        # We are done if everybody is happy.
-        if not someone_moved:
-            break
-
-    if someone_moved:
-        logging.info("No convergence achieved after {} iterations".format(model["max_iterations"]))
-
-    return locations_by_round
 
 
 if __name__ == "__main__":
     model_name = sys.argv[1]
     model = json.load(open(ppj("IN_MODEL_SPECS", model_name + ".json"), encoding="utf-8"))
 
-    logging.basicConfig(
-        filename=ppj("OUT_ANALYSIS", "log", "schelling_{}.log".format(model_name)),
-        filemode="w",
-        level=logging.INFO
-    )
-    np.random.seed(model["rng_seed"])
-    logging.info(model["rng_seed"])
+    #logging.basicConfig(
+    #    filename=ppj("OUT_ANALYSIS", "log", "schelling_{}.log".format(model_name)),
+    #    filemode="w",
+    #    level=logging.INFO
+    #)
+    #np.random.seed(1) #model["rng_seed"]
+    #logging.info(model[1]) #"rng_seed"
 
-    # Load initial locations and setup agents
-    agents = setup_agents(model)
-    # Run the main analysis
-    locations_by_round = run_analysis(agents, model)
-    # Store list with locations after each round
+    
+    lambda1 = model['penalty1']
+    lambda2 = model['penalty2']
+    y = np.loadtxt(ppj("OUT_DATA", "y.csv"), delimiter=",")
+    X = np.array(np.loadtxt(ppj("OUT_DATA", "x.csv"), delimiter=","))
+    X = np.reshape(X,[100,10])
+    beta = np.loadtxt(ppj("OUT_DATA", "beta.csv"), delimiter=",")
+    eps = np.loadtxt(ppj("OUT_DATA", "eps.csv"), delimiter=",")
+    aux = fused_lasso_primal(y,X,lambda1,lambda2)
     with open(ppj("OUT_ANALYSIS", "schelling_{}.pickle".format(model_name)), "wb") as out_file:
-        pickle.dump(locations_by_round, out_file)
+        pickle.dump(aux, out_file)
