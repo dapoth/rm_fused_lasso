1a7eb1521467aa039b2f4340b1171c11e1381c11
Christopher Thiemann <christopherth94@googlemail.com>
2019-02-15 15:58:33 +0100

    Modified the 'final' directory to include our files.

--- src/final/plot_locations.py
+++ src/final/plot_locations.py
@@ -11,33 +11,11 @@ from bld.project_paths import project_paths_join as ppj
 PLOT_ARGS = {"markersize": 4, "alpha": 0.6}
 
 
-def plot_locations(locations_by_round, model_name):
-    "Plot the distribution of agents after cycle_num rounds of the loop."
-    n_cycles = len(locations_by_round)
-    nrows = int(np.ceil(n_cycles / 2 - 0.01))
-    figsize = (2 * 3, nrows * 2)
-    fig, axes = plt.subplots(nrows=nrows, ncols=2, figsize=figsize)
-    fig.subplots_adjust(
-        left=0.05,
-        right=0.95,
-        bottom=0.05,
-        top=0.95,
-        wspace=0.25,
-        hspace=0.25
-    )
-    for item, ax in np.ndenumerate(axes):
-        n_cycle = item[0] * 2 + item[1]
-        if n_cycle == n_cycles:
-            # Remove last element if number of cycles is uneven
-            fig.delaxes(ax)
-            break
-        locs = locations_by_round[n_cycle]
-        ax.set_title("Cycle {}".format(n_cycle))
-        ax.tick_params(labelbottom="off", labelleft="off")
-        ax.set_facecolor("azure")
-        ax.plot(locs[0][:, 0], locs[0][:, 1], "o", markerfacecolor="orange", **PLOT_ARGS)
-        ax.plot(locs[1][:, 0], locs[1][:, 1], "o", markerfacecolor="green", **PLOT_ARGS)
-
+def plot_locations(aux, model_name):
+    
+    #fig = plt.subplot(np.linspace(0, 10, 10),aux)
+    fig = plt.figure()
+    plt.plot(range(1,11),aux)
     fig.savefig(ppj("OUT_FIGURES", "schelling_{}.png".format(model_name)))
 
 
@@ -47,6 +25,6 @@ if __name__ == "__main__":
 
     # Load locations after each round
     with open(ppj("OUT_ANALYSIS", "schelling_{}.pickle".format(model_name)), "rb") as in_file:
-        locations_by_round = pickle.load(in_file)
+        aux = pickle.load(in_file)
 
-    plot_locations(locations_by_round, model_name)
+    plot_locations(aux, model_name)
