16cbb8712d2109dee98c0ad5834e5ac153167b1d
ClaraBr <cl.bruenn@googlemail.com>
2019-03-03 15:00:16 +0100

    Added more tests and layouts for tests.

--- src/model_code/test_fused_lasso_dual.py
+++ src/model_code/test_fused_lasso_dual.py
@@ -15,7 +15,7 @@ import pytest
 expected_beta = [0, 2.5]
 
 @pytest.fixture
-def setup_param():
+def setup_param_signal():
     out_dual = {}
     out_dual['y'] = [0.25, 3]
     out_dual['x'] = np.identity(2)
@@ -31,12 +31,29 @@ def setup_param():
     out = {"data_dual": out_dual, "data_primal": out_primal}
     return out
 
-def test_lasso_dual(setup_param):
-    calculated_beta_dual = fused_lasso_dual(**setup_param["data_dual"])
+@pytest.fixture
+def setup_param():
+    out_dual = {}
+    out_dual['y'] = [0.25, 3]
+    out_dual["x"] = np.array([[[1, 0.5], [0.5, 1]]])
+    out_dual['lambda1'] = 1
+    out_dual['lambda2'] = 0
+
+    out_primal = {}
+    out_primal['y'] = [0.25, 3]
+    out_primal['x'] = np.array([[[1, 0.5], [0.5, 1]]])
+    out_primal['s1'] = 2.5
+    out_primal['s2'] = 100000
+    
+    out = {"data_dual": out_dual, "data_primal": out_primal}
+    return out
+    
+def test_lasso_signal_dual(setup_param_signal):
+    calculated_beta_dual = fused_lasso_dual(**setup_param_signal["data_dual"])
     assert_allclose(expected_beta, calculated_beta_dual, atol=1e-2)
     
-def test_lasso_primal(setup_param):
-    calculated_beta_primal = fused_lasso_primal(**setup_param["data_primal"])
+def test_lasso_signal_primal(setup_param_signal):
+    calculated_beta_primal = fused_lasso_primal(**setup_param_signal["data_primal"])
     assert_allclose(expected_beta, calculated_beta_primal, atol=1e-2)
+    
 
-#test_fused_lasso_dual([0.25,3], np.identity(2), 1, 0)
