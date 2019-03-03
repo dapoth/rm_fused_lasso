e19c927ec6a056c332bc3d241500fd7d0c5f0d1f
ClaraBr <cl.bruenn@googlemail.com>
2019-03-03 19:41:25 +0100

    Added code lines that check that fused_lasso_dual gets the right inputs.

--- src/model_code/fused_lasso_dual.py
+++ src/model_code/fused_lasso_dual.py
@@ -4,6 +4,12 @@ def fused_lasso_dual(y, x, lambda1, lambda2):
     """
     Solves for given data and penalty constants the fused lasso dual form.
     """
+    
+    if len(y) != len(x):
+        raise ValueError("The length of y must be equal to the number of rows of x.") 
+    #if lambda1 < 0 | lambda2 < 0:
+    #    raise ValueError("The penalty constraints need to be nonnnegative.")
+        
     p = len(x[1, :])
     b = cp.Variable(p)
     error = cp.sum_squares(x*b - y)
