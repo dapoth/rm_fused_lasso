3f06a790367a78ab322f7ca06855879d9aca90f3
Christopher Thiemann <christopherth94@googlemail.com>
2019-03-04 17:22:07 +0100

    Implemented 'numba' on the generate_blocks function.

--- src/sandbox/test.py
+++ src/sandbox/test.py
@@ -5,73 +5,156 @@ Created on Mon Feb 25 23:49:37 2019
 
 @author: clara
 """
-
-
-
-
-
+import math
+import random as rd
 import numpy as np
-import pickle
-import cvxpy as cp
+from numba import jit
+from time import time
 
-def fused_lasso_dual(y, x, lambda1, lambda2):
+
+num_features = 2000
+number_blocks =  3
+block_height= 3
+length_blocks= 9
+spikes = 3
+spike_height =  7
+levels =  1
+
+@jit(nopython=True)
+def generate_blocks(num_features, number_blocks, length_blocks, block_height,
+                    spike_height, levels=False, spikes=0):
+    """Generate non-overlapping *generate_blocks* for one simulation step.
+
+    Divide the generated_blocks into 1/*length_blocks* possible blocks and
+    randomly setting *number_blocks* of them non-zero.
+
+    Args:
+        num_features (int): number of features
+        number_blocks (int): non overlapping feature blocks to create
+        length_blocks (int): the length of feature blocks to create
+        block_height (int): height of basis feature blocks
+        spike_height (int): height of spikes
+        levels (boolean): indicate whether blocks should two different
+            possible heights
+        spikes (int): number of spikes to be added
+
+    Returns:
+        generated_blocks (np.ndarray)
+
     """
-    Solves for given data and penalty constants the fused lasso dual form.
+
+    generated_blocks = np.zeros(num_features)
+    max_blocks = np.floor(num_features / length_blocks)
+
+
+    start_blocks = np.random.choice(np.arange(max_blocks), number_blocks)
+
+
+#    if max_blocks < number_blocks:
+#        break
+
+    if levels:
+        """
+        If the Blocks should not all have equal levels, we will randomly chose
+        the level of each block as either block_height or block_height *2.
+        """
+        heights = np.array([block_height, block_height*2])
+        for block in start_blocks:
+            random_height = np.random.choice(heights)
+            for i in range(num_features):
+                if (i >= block * length_blocks) and (i < (block+1) *
+                                                     length_blocks):
+                    generated_blocks[i] = random_height
+
+    else:
+        for block in start_blocks:
+            for i in range(num_features):
+                if (i >= block * length_blocks) and (i < (block+1) *
+                                                     length_blocks):
+                    generated_blocks[i] = block_height
+    if spikes != 0:
+        non_blocks = np.arange(num_features-number_blocks * length_blocks - spikes)
+        for i in range(num_features):
+            if generated_blocks[i] == 0:
+                for j in range(len(non_blocks)):
+                    non_blocks[j] = i
+        beta_spikes = np.random.choice(non_blocks, spikes)
+        for k in range(len(beta_spikes)):
+            beta_spikes[k] = int(beta_spikes[k])
+        for i in beta_spikes:
+            generated_blocks[i] = spike_height
+
+    return generated_blocks
+
+start_time_new = time()
+generate_blocks_new(num_features, number_blocks, length_blocks, block_height, spike_height, levels, spikes)
+end_time_new = time()
+
+print( end_time_new-start_time_new)
+
+
+def generate_blocks(num_features, number_blocks, length_blocks, block_height,
+                    spike_height, levels=False, spikes=0):
+    """Generate non-overlapping *generate_blocks* for one simulation step.
+
+    Divide the generated_blocks into 1/*length_blocks* possible blocks and
+    randomly setting *number_blocks* of them non-zero.
+
+    Args:
+        num_features (int): number of features
+        number_blocks (int): non overlapping feature blocks to create
+        length_blocks (int): the length of feature blocks to create
+        block_height (int): height of basis feature blocks
+        spike_height (int): height of spikes
+        levels (boolean): indicate whether blocks should two different
+            possible heights
+        spikes (int): number of spikes to be added
+
+    Returns:
+        generated_blocks (np.ndarray)
+
     """
-    p = len(x[1, :])
-    b = cp.Variable(p)
-    error = cp.sum_squares(x*b - y)
-    obj = cp.Minimize(error + lambda1 * cp.norm(b, 1) +
-                      lambda2 * cp.norm(b[1:p]-b[0:p-1], 1))
-    prob = cp.Problem(obj)
-    prob.solve()
+    generated_blocks = np.zeros(num_features)
+    max_blocks = math.floor(num_features / length_blocks)
 
-    return b.value
+    start_blocks = rd.sample(range(max_blocks), number_blocks)
 
-def fused_lasso_primal(y, x, s1, s2):
+#    if max_blocks < number_blocks:
+#        break
 
-    """Docstring."""
+    if levels:
+        """
+        If the Blocks should not all have equal levels, we will randomly chose
+        the level of each block as either block_height or block_height *2.
+        """
+        heights = [block_height, block_height*2]
+        for block in start_blocks:
+            random_height = rd.choice(heights)
+            for i in range(num_features):
+                if (i >= block * length_blocks) and (i < (block+1) *
+                                                     length_blocks):
+                    generated_blocks[i] = random_height
 
-    # "from constraint import un_constraint" to import function
-    # y and x data as usual
+    else:
+        for block in start_blocks:
+            for i in range(num_features):
+                if (i >= block * length_blocks) and (i < (block+1) *
+                                                     length_blocks):
+                    generated_blocks[i] = block_height
 
-    p = len(x[1, :])
-    b = cp.Variable(p)
-    error = cp.sum_squares(x*b - y)
-    obj = cp.Minimize(error)
-    constraints = [cp.norm(b, 1) <= s1, cp.norm(b[1:p]-b[0:p-1], 1) <= s2]
-    prob = cp.Problem(obj, constraints)
-    prob.solve()
+    if spikes != 0:
+        non_blocks = []
+        for i in range(num_features):
+            if generated_blocks[i] == 0:
+                non_blocks.append(i)
+        beta_spikes = rd.sample(non_blocks, spikes)
+        for i in beta_spikes:
+            generated_blocks[i] = spike_height
 
-    return b.value
+    return generated_blocks
 
-def calc_lambda(y, X, s1, beta_s):
+start_time_old = time()
+generate_blocks(num_features, number_blocks, length_blocks, block_height, spike_height, levels, spikes)
+end_time_old = time()
 
-    lambda1 = 0
-    beta_l = fused_lasso_dual(y, X, lambda1, 0)
-
-    while np.sum(beta_l-beta_s) > 0.0000001:
-
-        lambda1 = lambda1 + 0.05
-
-        beta_l = fused_lasso_dual(y, X, lambda1, 0)
-
-        print(np.sum(beta_l-beta_s))
-
-        if lambda1 > 100:
-            break
-
-    return lambda1
-
-
-with open("/home/christopher/Dokumente/rm_fused_lasso/bld/out/data/data_spikes.pickle", "rb") as in12_file:
-    beta_X_epsilon_Y = pickle.load(in12_file)
-
-X = beta_X_epsilon_Y[1]
-y = beta_X_epsilon_Y[3][:,1]
-
-s1 = 140
-
-init = fused_lasso_primal(y,X,s1,0)
-
-test = calc_lambda(y,X,140,init)
+print(end_time_old-start_time_old)
