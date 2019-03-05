e78b9f119273ab8998b42bf09fcad15e0c14d23f
dapoth <davidpoth93@gmail.com>
2019-03-05 16:23:51 +0100

    Added docstrings  and adjusted docstyle for create_analysis_table.

--- src/final/cgh_plot.py
+++ src/final/cgh_plot.py
@@ -8,17 +8,19 @@ from src.model_code.fused_lasso_primal import fused_lasso_primal
 
 if __name__ == "__main__":
 
+    #Load Penalty constants and CGH data and estimate fused lasso results.
     PIC_DICT = json.load(open(ppj("IN_MODEL_SPECS", "picture_cgh.json"), encoding="utf-8"))
     CGH_DATA = np.loadtxt(ppj("IN_DATA", "cgh.txt"))
     BETA = fused_lasso_primal(CGH_DATA, np.identity(len(CGH_DATA)), PIC_DICT["s1"], PIC_DICT["s2"])
 
-
+    # Plot CGH data.
     plt.xlabel('Genome Order')
     plt.ylabel('Copy Number')
     plt.plot(CGH_DATA, "bo")
     plt.axhline(color='r')
     plt.savefig(ppj("OUT_FIGURES", "cgh_plot_raw.png"))
 
+    # Plot CGH data with fused lasso estimates.
     plt.xlabel('Genome Order')
     plt.ylabel('Copy Number')
     plt.plot(CGH_DATA, "bo")
--- src/final/create_analysis_table.py
+++ src/final/create_analysis_table.py
@@ -1,48 +1,39 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Feb 18 11:55:21 2019
-@author: clara
-"""
-
-from tabulate import tabulate
+"""Load results from simulation analysis and save them as table."""
 import pickle
-import numpy as np
 import pandas as pd
 from bld.project_paths import project_paths_join as ppj
 
-"""Load analysis data"""
-
-
- #data = {'item1': df_oneblock, 'item2': blocks}
-
-list = []
+# Load results from simulation analysis and add them to the list RESULTS.
+RESULTS = []
 for sim in 'large_blocks', 'blocks_few_spikes', 'small_blocks', 'spikes':
     for reg in 'lasso', 'fused', 'fusion':
-          with open(ppj("OUT_ANALYSIS", "analysis_{}_{}.pickle".format(reg, sim)), "rb") as in12_file:
-              analysis = pickle.load(in12_file)
-              analysis.append(sim)
-              analysis.append(reg)
-              list.append(analysis)
-list_pd = pd.DataFrame(list).round(2)
-list_pd
+        with open(ppj("OUT_ANALYSIS", "analysis_{}_{}.pickle".format(reg, sim)), "rb") as in12_file:
+            analysis = pickle.load(in12_file)
+            analysis.append(sim)
+            analysis.append(reg)
+            RESULTS.append(analysis)
+RESULTS_PD = pd.DataFrame(RESULTS).round(2)
+RESULTS_PD
 
-for i in [1,3,5,7]:
+# Add '(' and ')' around estimated standard errors.
+COL_STD_ERR = [1, 3, 5, 7]
+for i in COL_STD_ERR:
     for j in range(12):
+        RESULTS_PD.iloc[j, i] = '(' + str(RESULTS_PD.iloc[j, i]) + ')'
 
-        list_pd.iloc[j,i] = '(' + str(list_pd.iloc[j,i]) + ')'
-
-idx_rename = {0:'Lasso', 1:'Fused', 2: 'Fusion',3:'Lasso', 4:'Fused', 5: 'Fusion',6:'Lasso', 7:'Fused', 8: 'Fusion',9:'Lasso', 10:'Fused', 11: 'Fusion'}
-columns_rename = {0:"sensitivity" ,1:""  ,2:"specificity", 3:"" , 4:"spikes", 5:"" ,6:"blocks",7:"",8:"Setting",9:"Method"}
-list_pd = list_pd.rename( columns=columns_rename)
-
-
-
-
-list_pd = list_pd.set_index(["Setting","Method"])
+IDX_NAME = {0:'Lasso', 1:'Fused', 2: 'Fusion', 3:'Lasso', 4:'Fused', 5: 'Fusion',
+            6:'Lasso', 7:'Fused', 8: 'Fusion', 9:'Lasso', 10:'Fused', 11: 'Fusion'}
+COLUMN_NAME = {0:"sensitivity", 1:"", 2:"specificity", 3:"", 4:"spikes", 5:"",
+               6:"blocks", 7:"", 8:"Setting", 9:"Method"}
+RESULTS_PD = RESULTS_PD.rename(columns=COLUMN_NAME)
 
 
-list_pd.to_latex()
+
+
+RESULTS_PD = RESULTS_PD.set_index(["Setting", "Method"])
+
+
+RESULTS_PD.to_latex()
 
 with open(ppj("OUT_FIGURES", "mytable.tex"), 'w') as tf:
-     tf.write(list_pd.to_latex())
+    tf.write(RESULTS_PD.to_latex())
