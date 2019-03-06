d6e78fb0e9e281dbabc82683062c6f38722d12a9
dapoth <davidpoth93@gmail.com>
2019-03-06 22:41:11 +0100

    Cleaned analysis and adapted variables.

--- src/analysis/grid_cross_validation.py
+++ src/analysis/grid_cross_validation.py
@@ -1,118 +1,100 @@
 import sys
 import json
-import logging
 import pickle
-import numpy as np
-from src.model_code.flestimator import FusedLassoEstimator as fle
-from bld.project_paths import project_paths_join as ppj
-from sklearn.model_selection import GridSearchCV
 from time import time
+import numpy as np
+from sklearn.model_selection import GridSearchCV
+from bld.project_paths import project_paths_join as ppj
+from src.model_code.flestimator import FusedLassoEstimator as fle
 
 if __name__ == "__main__":
     """Waf."""
-    sim_name = sys.argv[2]
-    reg_name = sys.argv[1]
+    SIM_NAME = sys.argv[2]
+    REG_NAME = sys.argv[1]
 
-    sim_dict = json.load(open(ppj("IN_MODEL_SPECS", sim_name + ".json"),
+    SIM_DICT = json.load(open(ppj("IN_MODEL_SPECS", SIM_NAME + ".json"),
                               encoding="utf-8"))
 
     with open(ppj("OUT_DATA", "data_{}.pickle".
-              format(sim_name)), "rb") as in12_file:
-        beta_X_epsilon_Y = pickle.load(in12_file)
+                  format(SIM_NAME)), "rb") as in_file:
+        BETA_X_EPSILON_Y = pickle.load(in_file)
 
 
-    """Pull Information out of json file."""
-    s1_min = sim_dict["s1_min"]
-    s1_max = sim_dict["s1_max"]
-    s2_min = sim_dict["s2_min"]
-    s2_max = sim_dict["s2_max"]
-    grid_density = sim_dict['grid_density']
+    #Pull Information out of json file.
+    N_FEATURES = SIM_DICT["p"]
+    N_OBS = SIM_DICT["n"]
+    S1_MIN = SIM_DICT["s1_min"]
+    S1_MAX = SIM_DICT["s1_max"]
+    S2_MIN = SIM_DICT["s2_min"]
+    S2_MAX = SIM_DICT["s2_max"]
+    GRID_DENSITY = SIM_DICT['grid_density']
 
     """Data import from pickle files."""
-    true_beta = beta_X_epsilon_Y[0]                 # p x num_simulations
-    X = beta_X_epsilon_Y[1]                         # n x p
-    epsilon = beta_X_epsilon_Y[2]                   # n x num_simulations
-    y = beta_X_epsilon_Y[3]
-
-    """Pull Information out of json file."""
-    p = sim_dict["p"]
-    n = sim_dict["n"]
-    s1_min = sim_dict["s1_min"]
-    s1_max = sim_dict["s1_max"]
-    s2_min = sim_dict["s2_min"]
-    s2_max = sim_dict["s2_max"]
-    grid_density = sim_dict['grid_density']
-    num_simulations = sim_dict['num_simulations']  # how many times simulation gets run
-
-    """Building containers tused_so store simulation results."""
-    beta_hat = np.empty((p, num_simulations))
-    y_hat = np.empty((n, num_simulations))
-    residuals = np.empty((n, num_simulations))
-
-    """Calculation of optimal lambda (still missing)."""
+    X = BETA_X_EPSILON_Y[1]                         # n x p
+    y = BETA_X_EPSILON_Y[3]
 
 
-    lasso_grid = {
-      's1': list(np.linspace(s1_min, s1_max, grid_density))
+    """Assessing of optimal lambda."""
+
+    LASSO_GRID = {
+        's1': list(np.linspace(S1_MIN, S1_MAX, GRID_DENSITY))
     }
-    fused_grid = {
-      's2': list(np.linspace(s2_min, s2_max, grid_density))
+    FUSION_GRID = {
+        's2': list(np.linspace(S2_MIN, S2_MAX, GRID_DENSITY))
     }
-    two_d_grid = [{
-            's1': list(np.linspace(s1_min, s1_max, grid_density)),
-            's2': list(np.linspace(s2_min, s2_max, grid_density))
-        }]
+    TWO_D_GRID = [{
+        's1': list(np.linspace(S1_MIN, S1_MAX, GRID_DENSITY)),
+        's2': list(np.linspace(S2_MIN, S2_MAX, GRID_DENSITY))
+    }]
 
-    if reg_name == 'lasso':
-        lasso_grid = {
-            's1': list(np.linspace(s1_min, s1_max, grid_density))
+    if REG_NAME == 'lasso':
+        LASSO_GRID = {
+            's1': list(np.linspace(S1_MIN, S1_MAX, GRID_DENSITY))
         }
-        fused_grid = {
+        FUSION_GRID = {
             's2': list(np.linspace(1190, 1200, 1))
         }
 
-        two_d_grid = [{
-            's1': list(np.linspace(s1_min, s1_max, grid_density)),
+        TWO_D_GRID = [{
+            's1': list(np.linspace(S1_MIN, S1_MAX, GRID_DENSITY)),
             's2': list(np.linspace(1190, 1200, 1))
         }]
 
-    if reg_name == 'fusion':
-        lasso_grid = {
+    if REG_NAME == 'fusion':
+        LASSO_GRID = {
             's1': list(np.linspace(1190, 1200, 1))
         }
-        fused_grid = {
-            's2': list(np.linspace(s2_min, s2_max, grid_density))
+        FUSION_GRID = {
+            's2': list(np.linspace(S2_MIN, S2_MAX, GRID_DENSITY))
         }
-        two_d_grid = [{
+        TWO_D_GRID = [{
             's1': list(np.linspace(1190, 1200, 1)),
-            's2': list(np.linspace(s2_min, s2_max, grid_density))
+            's2': list(np.linspace(S2_MIN, S2_MAX, GRID_DENSITY))
         }]
 
-    start_time_cv = time()
 
-    clf = GridSearchCV(fle(lasso_grid, fused_grid), two_d_grid,
+    START_TIME_CV = time()
+    CLF = GridSearchCV(fle(LASSO_GRID, FUSION_GRID), TWO_D_GRID,
                        scoring='neg_mean_squared_error',
                        n_jobs=-1, iid=False, refit=True,
                        cv=3, verbose=0, pre_dispatch='2*n_jobs',
                        error_score='raise-deprecating',
                        return_train_score='warn')
 
+    CLF.fit(X, y[:, 1])
+    PENALTY_CV = [CLF.best_params_["s1"], CLF.best_params_["s2"]]
+    END_TIME_CV = time()
 
 
-    clf.fit(X, y[:, 1])
-    penalty_cv = [clf.best_params_["s1"], clf.best_params_["s2"]]
-    end_time_cv = time()
+    if REG_NAME == 'fused':
+
+        TIMINGS_FUSED_LASSO = [N_FEATURES, N_OBS, np.round((END_TIME_CV - START_TIME_CV), 2)]
+
+        with open(ppj("OUT_ANALYSIS", "time_list_{}.pickle".
+                      format(SIM_NAME)), "wb") as out_file:
+            pickle.dump(TIMINGS_FUSED_LASSO, out_file)
 
 
-
-    if reg_name == 'fused':
-
-        time_list = [p, n, np.round((end_time_cv-start_time_cv), 2)]
-
-        with open(ppj("OUT_ANALYSIS", "time_list_{}.pickle".format(sim_name)), "wb") as out_file:
-           pickle.dump(time_list, out_file)
-
-
-    container = [clf.cv_results_['mean_test_score'], clf.cv_results_['params'], penalty_cv]
-    with open(ppj("OUT_ANALYSIS", "cv_{}_{}.pickle".format(reg_name, sim_name)), "wb") as out_file:
-        pickle.dump(container, out_file)
+    CONTAINER = [CLF.cv_results_['mean_test_score'], CLF.cv_results_['params'], PENALTY_CV]
+    with open(ppj("OUT_ANALYSIS", "cv_{}_{}.pickle".format(REG_NAME, SIM_NAME)), "wb") as out_file:
+        pickle.dump(CONTAINER, out_file)
--- src/analysis/monte_carlo.py
+++ src/analysis/monte_carlo.py
@@ -1,72 +1,63 @@
-import sys
 import json
 import pickle
 import numpy as np
-from src.model_code.fused_lasso_primal import fused_lasso_primal
-from sklearn.model_selection import GridSearchCV
-from bld.project_paths import project_paths_join as ppj
 import matplotlib.pyplot as plt
+from src.model_code.fused_lasso_primal import fused_lasso_primal
+from bld.project_paths import project_paths_join as ppj
 
 if __name__ == "__main__":
     """ waf """
 
-    with open(ppj("OUT_ANALYSIS", "simulation_fused_large_blocks.pickle"), "rb") as in12_file:
-                  analysis = pickle.load(in12_file)
+    with open(ppj("OUT_ANALYSIS", "simulation_fused_large_blocks.pickle"), "rb") as in_file:
+        ANALYSIS = pickle.load(in_file)
 
-    sim_dict = json.load(open(ppj("IN_MODEL_SPECS", "large_blocks.json"),
-                             encoding="utf-8"))
-
+    SIM_DICT = json.load(open(ppj("IN_MODEL_SPECS", "large_blocks.json"),
+                              encoding="utf-8"))
 
     """Plot distribution."""
-    beta = analysis[1][:,1]   #true_beta[:, 1]  # nimm ein echtes beta
-    penalty_cv = analysis[2]
+    BETA = ANALYSIS[1][:, 1]   #true_beta[:, 1]  # nimm ein echtes beta
+    S1_OPT = ANALYSIS[2][0]
+    S2_OPT = ANALYSIS[2][1]
 
+    N_FEATURES = SIM_DICT["p"]
+    N_OBS = SIM_DICT["n"]
+    N_SIMULATIONS = SIM_DICT['num_simulations']
 
+    BETA_HAT_CONTAINER = np.ones((N_FEATURES, N_SIMULATIONS))
+    MEAN_X = np.zeros(N_FEATURES)
+    COV_X = np.identity(N_FEATURES)
+    X = np.random.multivariate_normal(MEAN_X, COV_X, N_OBS)
 
-    p = sim_dict["p"]
-    n = sim_dict["n"]
-    s1_min = sim_dict["s1_min"]
-    s1_max = sim_dict["s1_max"]
-    s2_min = sim_dict["s2_min"]
-    s2_max = sim_dict["s2_max"]
-    num_simulations = sim_dict['num_simulations']
-
-    beta_container = np.ones((p, num_simulations))
-    mean_x = np.zeros(p)
-    cov_X = np.identity(p)
-    X = np.random.multivariate_normal(mean_x, cov_X, n)
-
-    for j in range(num_simulations):
-
-        eps = np.random.rand(n)
-        y = np.matmul(X, beta) + eps
-        beta_container[:, j] = fused_lasso_primal(y, X, penalty_cv[0], penalty_cv[1])
+    for j in range(N_SIMULATIONS):
+        eps = np.random.rand(N_OBS)
+        y = np.matmul(X, BETA) + eps
+        BETA_HAT_CONTAINER[:, j] = fused_lasso_primal(y, X, S1_OPT, S2_OPT)
 
 
 
     """Plot distribution of beta_j before block at break of block and inside block"""
-    list_index = []
-    for ch in range(p):
-        if ch == (p-4):
-            list_index = [4, 7, 9, 12]
+    LIST_INDEX = []
+    for i in range(N_FEATURES):
+        if i == (N_FEATURES-4):
+            LIST_INDEX = [4, 7, 9, 12]
             break
 
-        if (beta[ch] == 0) & (beta[(ch+1)] == 0) & (beta[(ch+2)] != 0) & (beta[(ch+3)] != 0):
-            list_index = [ch+3, ch, ch+2, ch+1]
+        if (BETA[i] == 0) & (BETA[(i+1)] == 0) & (BETA[(i+2)] != 0) & (BETA[(i+3)] != 0):
+            LIST_INDEX = [i+3, i, i+2, i+1]
             break
 
-    fig, axes = plt.subplots(2, 2)
+    FIG, AXES = plt.subplots(2, 2)
 
-    axes[0, 0].set_title('center')
-    axes[0, 0].hist(beta_container[list_index[0], :])
+    AXES[0, 0].set_title('center')
+    AXES[0, 0].hist(BETA_HAT_CONTAINER[LIST_INDEX[0], :])
 
-    axes[1, 0].set_xlabel('zero')
-    axes[1, 0].hist(beta_container[list_index[1], :])
+    AXES[1, 0].set_xlabel('zero')
+    AXES[1, 0].hist(BETA_HAT_CONTAINER[LIST_INDEX[1], :])
 
-    axes[0, 1].hist(beta_container[list_index[2], :])
-    axes[0, 1].set_title('block_in')
+    AXES[0, 1].hist(BETA_HAT_CONTAINER[LIST_INDEX[2], :])
+    AXES[0, 1].set_title('block_in')
 
-    axes[1, 1].hist(beta_container[list_index[3], :])
-    axes[1, 1].set_xlabel('block_out')
+    AXES[1, 1].hist(BETA_HAT_CONTAINER[LIST_INDEX[3], :])
+    AXES[1, 1].set_xlabel('block_out')
 
     plt.savefig(ppj("OUT_FIGURES", "monte_carlo_fused_large_blocks.png"))
