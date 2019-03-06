8137e1711d17ab1d37c31a245b1dbe4395dc4895
dapoth <davidpoth93@gmail.com>
2019-03-06 15:31:33 +0100

    Renamed and regrouped data generation process.

--- src/data_management/generate_data.py
+++ src/data_management/generate_data.py
@@ -1,5 +1,68 @@
+import math
+import random as rd
 import numpy as np
-from generate_blocks import generate_blocks
+#from numba import jit
+
+def generate_beta(num_features, number_blocks, length_blocks, block_height,
+                  levels=False, spikes=0, spike_height=7):
+    """Generate beta with non-overlapping blocks.
+
+    Divide the generated_blocks into 1/*length_blocks* possible blocks and
+    randomly setting *number_blocks* of them non-zero.
+
+    Args:
+        num_features (int): number of features
+        number_blocks (int): non overlapping feature blocks to create
+        length_blocks (int): the length of feature blocks to create
+        block_height (int): height of basis feature blocks
+        spike_height (int): height of spikes
+        levels (boolean): indicate whether blocks should two different
+            possible heights
+        spikes (int): number of spikes to be added
+
+    Returns:
+        generated_blocks (np.ndarray)
+
+    """
+    max_blocks = math.floor(num_features / length_blocks)
+    if max_blocks < number_blocks:
+        raise TypeError("""The number of blocks must not exceed the maximal number
+                        of blocks possible for given p and length of blocks.""")
+    generated_blocks = np.zeros(num_features)
+
+    start_blocks = rd.sample(range(max_blocks), number_blocks)
+
+    if levels:
+        """
+        If the Blocks should not all have equal levels, we will randomly chose
+        the level of each block as either block_height or block_height *2.
+        """
+        heights = [block_height, block_height*2]
+        for block in start_blocks:
+            random_height = rd.choice(heights)
+            for i in range(num_features):
+                if (i >= block * length_blocks) and (i < (block+1) *
+                                                     length_blocks):
+                    generated_blocks[i] = random_height
+
+    else:
+        for block in start_blocks:
+            for i in range(num_features):
+                if (i >= block * length_blocks) and (i < (block+1) *
+                                                     length_blocks):
+                    generated_blocks[i] = block_height
+
+    if spikes != 0:
+        non_blocks = []
+        for i in range(num_features):
+            if generated_blocks[i] == 0:
+                non_blocks.append(i)
+        beta_spikes = rd.sample(non_blocks, spikes)
+        for i in beta_spikes:
+            generated_blocks[i] = spike_height
+
+    return generated_blocks
+
 
 
 def generate_data(num_simulations, num_observations, num_features, num_blocks,
@@ -25,8 +88,8 @@ def generate_data(num_simulations, num_observations, num_features, num_blocks,
     """
     beta = np.zeros([num_features, num_simulations])
     for sim in range(num_simulations):
-        beta[:, sim] = generate_blocks(num_features, num_blocks, length_blocks,
-                                       height, spike_height, levels, spikes)
+        beta[:, sim] = generate_beta(num_features, num_blocks, length_blocks,
+                                     height, levels, spikes, spike_height)
     mean = np.zeros(num_features)
     cov = np.identity(num_features)
     X = np.random.multivariate_normal(mean, cov, num_observations)
--- src/data_management/test_data_generation.py
+++ src/data_management/test_data_generation.py
@@ -7,7 +7,7 @@ Created on Tue Mar  5 23:30:29 2019
 """
 
 import numpy
-from src.data_management.generate_blocks import generate_blocks
+from src.data_management.generate_data import generate_beta
 import pytest
 
 @pytest.fixture
@@ -19,20 +19,19 @@ def setup_block_generation():
     out['block_height'] = 3
     out['spike_height'] = 0
     return out
-    
+
 @pytest.fixture
 def expected_output():
     out = {}
     out['nonzeros'] = 9
     return out
-    
-def test_generate_blocks(setup_block_generation):
-    beta_hat = generate_blocks(**setup_block_generation)
+
+def test_generate_beta(setup_block_generation):
+    beta_hat = generate_beta(**setup_block_generation)
     number_of_nonzeros = numpy.count_nonzero(beta_hat)
     numpy.testing.assert_array_equal(9, number_of_nonzeros)
-    
+
 def test_generate_too_many_blocks(setup_block_generation):
     setup_block_generation['number_blocks'] = 4
     with pytest.raises(TypeError):
-        generate_blocks(**setup_block_generation)
-
+        generate_beta(**setup_block_generation)
