f7a10881d8fde363ba8c34ad81ffae1af05853a3
ClaraBr <cl.bruenn@googlemail.com>
2019-03-06 22:59:37 +0100

    Added test for the correct dimensionality of the generate_data function.

--- src/data_management/test_data_generation.py
+++ src/data_management/test_data_generation.py
@@ -2,17 +2,33 @@
 # -*- coding: utf-8 -*-
 import numpy
 from src.data_management.generate_data import generate_beta
+from src.data_management.generate_data import generate_data
 import pytest
 import sys
 
 @pytest.fixture
-def setup_block_generation():
+def setup_beta_generation():
     out = {}
     out['num_features'] = 10
     out['number_blocks'] = 2
     out['length_blocks'] = 3
     out['block_height'] = 3
     return out
+                      
+@pytest.fixture
+def setup_data_generation():
+    out = {}
+    out['num_simulations'] = 100
+    out['num_observations'] = 20
+    out['num_features'] = 10
+    out['num_blocks'] = 2
+    out['length_blocks'] = 3
+    out['height'] = 3
+    out['levels'] = True
+    out['spikes'] = 3
+    out['spike_height'] = 5
+    return out
+
 
 @pytest.fixture
 def expected_output():
@@ -20,31 +36,51 @@ def expected_output():
     out['blocks'] = 6
     out['blocks_with_spikes'] = 8
     return out
+
+@pytest.fixture
+def expected_dimensions():
+    out = {}
+    out['beta'] = (10,100)
+    out['X'] = (20,10)
+    out['epsilon'] = (20, 100)
+    out['y'] = (20, 100)
+    out_dimensions = [out['beta'], out['X'], out['epsilon'], out['y']]
+    return out_dimensions
     
-def test_generate_beta(setup_block_generation, expected_output):
-    beta_hat = generate_beta(**setup_block_generation)
+def test_generate_beta(setup_beta_generation, expected_output):
+    beta_hat = generate_beta(**setup_beta_generation)
     number_of_nonzeros = numpy.count_nonzero(beta_hat)
     expected_nonzeros = expected_output['blocks']
     numpy.testing.assert_array_equal(expected_nonzeros, number_of_nonzeros)
     
-def test_generate_beta_with_levels(setup_block_generation, expected_output):
-    setup_block_generation['levels'] = True
-    beta_hat = generate_beta(**setup_block_generation)
+def test_generate_beta_with_levels(setup_beta_generation, expected_output):
+    setup_beta_generation['levels'] = True
+    beta_hat = generate_beta(**setup_beta_generation)
     number_of_nonzeros = numpy.count_nonzero(beta_hat)
     expected_nonzeros = expected_output['blocks']
     numpy.testing.assert_array_equal(expected_nonzeros, number_of_nonzeros)
     
-def test_generate_beta_with_spikes(setup_block_generation, expected_output):
-    setup_block_generation['spikes'] = 2
-    beta_hat = generate_beta(**setup_block_generation)
+def test_generate_beta_with_spikes(setup_beta_generation, expected_output):
+    setup_beta_generation['spikes'] = 2
+    beta_hat = generate_beta(**setup_beta_generation)
     number_of_nonzeros = numpy.count_nonzero(beta_hat)
     expected_nonzeros = expected_output['blocks_with_spikes']
     numpy.testing.assert_array_equal(expected_nonzeros, number_of_nonzeros)
 
-def test_generate_too_many_blocks(setup_block_generation):
-    setup_block_generation['number_blocks'] = 4
+def test_generate_too_many_blocks(setup_beta_generation):
+    setup_beta_generation['number_blocks'] = 4
     with pytest.raises(TypeError):
-        generate_beta(**setup_block_generation)
+        generate_beta(**setup_beta_generation)
+        
+def test_dimensionality_output_data_generation(setup_data_generation, expected_dimensions):
+    beta, X, epsilon, y = generate_data(**setup_data_generation)
+    dim_beta = beta.shape
+    dim_X = X.shape
+    dim_epsilon = epsilon.shape
+    dim_y = y.shape
+    dimension = [dim_beta, dim_X, dim_epsilon, dim_y]
+    numpy.testing.assert_array_equal(dimension, expected_dimensions)
+    
 
 
 if __name__== '__main__':
